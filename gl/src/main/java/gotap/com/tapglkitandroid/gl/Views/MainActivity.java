9d98b6d83890060e0b640192ab5787cfff0c893c
Morgot <Morgot@HellInMInd.local>
2017-04-25 16:05:43 +0300

    comment fck ile

--- gl/src/main/java/gotap/com/tapglkitandroid/gl/Views/MainActivity.java
+++ gl/src/main/java/gotap/com/tapglkitandroid/gl/Views/MainActivity.java
@@ -1,46 +1,46 @@
-package gotap.com.tapglkitandroid.gl.Views;
-
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-
-import gotap.com.tapglkitandroid.gl.R;
-
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final TapLoadingView view = (TapLoadingView)findViewById(R.id.tapLoading);
-
-        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                view.start();
-            }
-        });
-        findViewById(R.id.pause).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                view.pause();
-            }
-        });
-        findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                view.forceStop = !view.forceStop;
-            }
-        });
-        findViewById(R.id.changeColor).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                view.useCustomColor = !view.useCustomColor;
-            }
-        });
-
-    }
-
-}
+//package gotap.com.tapglkitandroid.gl.Views;
+//
+//
+//import android.app.Activity;
+//import android.os.Bundle;
+//import android.view.View;
+//
+//import gotap.com.tapglkitandroid.gl.R;
+//
+//public class MainActivity extends Activity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        final TapLoadingView view = (TapLoadingView)findViewById(R.id.tapLoading);
+//
+//        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                view.start();
+//            }
+//        });
+//        findViewById(R.id.pause).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                view.pause();
+//            }
+//        });
+//        findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                view.forceStop = !view.forceStop;
+//            }
+//        });
+//        findViewById(R.id.changeColor).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                view.useCustomColor = !view.useCustomColor;
+//            }
+//        });
+//
+//    }
+//
+//}
 
