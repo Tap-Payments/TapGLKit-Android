e925e57a2fb17b8f4198dcbfbd40d46976fd386e
HaithamSheshtawy <h.sheshtawy@tap.company>
2020-03-04 16:36:05 +0300

    updating compileSdkVersion to 29

---
+++ gl/build/generated/source/buildConfig/debug/gotap/com/tapglkitandroid/gl/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package gotap.com.tapglkitandroid.gl;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "gotap.com.tapglkitandroid.gl";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "2.0";
+}
